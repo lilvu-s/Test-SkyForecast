2f3d86bb25453db265a0f9285c9f493a0e2726a6
Angelina <>
2023-12-16 07:51:53 +0200

    Added low visibility logic

--- SkyForecast/Controllers/DetailedWeatherTableViewController.swift
+++ SkyForecast/Controllers/DetailedWeatherTableViewController.swift
@@ -10,39 +10,62 @@ import Foundation
 import UIKit
 
 class DetailedWeatherTableViewController: UITableViewController {
-    static let identifier = "DetailedWeatheTableViewController"
-
-    //MARK: -
-    @IBOutlet weak var highestTempratureLabel: UILabel!
-    @IBOutlet weak var lowestTempratureLabel: UILabel!
+    //MARK: - Outlets
+    @IBOutlet weak var highestTemperatureLabel: UILabel!
+    @IBOutlet weak var lowestTemperatureLabel: UILabel!
     
     @IBOutlet weak var windSpeedLabel: UILabel!
     @IBOutlet weak var visibilityLabel: UILabel!
-
-    var object: DailyWeather!
     
-    //MARK: -
+    var model: DailyWeather?
+    var isLowVisibility: Bool = false
+    
+    // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.tableView.tableFooterView = UIView.init()
+        updateWeatherDisplay()
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        DispatchQueue.main.async {
-            self.setupWithObject()
+    // MARK: - Setup
+    private func updateWeatherDisplay() {
+        guard let weather = model else { return }
+        
+        setLabelValues(with: weather)
+        isLowVisibility = weather.visibility < 10
+        
+        updateTableHeader(for: weather.visibility)
+        tableView.reloadData()
+    }
+    
+    private func setLabelValues(with weather: DailyWeather) {
+        highestTemperatureLabel.text = "The highest temperature is \(weather.tempmax)"
+        lowestTemperatureLabel.text = "The lowest temperature is \(weather.tempmin)"
+        windSpeedLabel.text = "Wind speed is around \(weather.windspeed)"
+    }
+    
+    private func updateTableHeader(for visibility: Double) {
+        if isLowVisibility {
+            visibilityLabel.textColor = .red
+            visibilityLabel.font = .systemFont(ofSize: 20, weight: .semibold)
+            visibilityLabel.text = "⚠️ Visibility \(visibility)"
+            tableView.tableHeaderView = visibilityLabel
+            
+            visibilityLabel.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                visibilityLabel.topAnchor.constraint(equalTo: tableView.topAnchor, constant: 10),
+                visibilityLabel.leadingAnchor.constraint(equalTo: tableView.leadingAnchor, constant: 16),
+                visibilityLabel.widthAnchor.constraint(equalTo: tableView.widthAnchor)
+            ])
+        } else {
+            visibilityLabel.text = "Visibility \(visibility)"
+            tableView.tableHeaderView = nil
         }
     }
     
-    //MARK: - Private
-    private func setupWithObject() {
-        highestTempratureLabel.text = "The highest temprature is \(object.tempmax)"
-        lowestTempratureLabel.text = "The lowest temprature is \(object.tempmin)"
-        
-        windSpeedLabel.text = "Wind speed is aroud \(object.windspeed)"
-        visibilityLabel.text = "Visibility \(object.visibility)"
-
-        self.title = "Forecast"
+    //MARK: - TableView DataSource
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return isLowVisibility ? 3 : 4
     }
 }
+
